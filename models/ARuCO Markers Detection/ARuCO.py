a9f70e585ead1f927acd17ef7cc8d7c6e70b76f8
Sandeep Suresh <68272706+SandyTheAdventurer@users.noreply.github.com>
2025-06-07 10:11:27 +0530

    Forked the husarion_ugv_ros

---
+++ models/ARuCO Markers Detection/ARuCO.py
@@ -0,0 +1,29 @@
+import cv2
+import cv2.aruco as aruco
+
+image = cv2.imread('marker_images/test3.jpg') 
+if image is None:
+    raise FileNotFoundError("Image not found.")
+
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+parameters = aruco.DetectorParameters()
+
+detector = aruco.ArucoDetector(aruco_dict, parameters)
+corners, ids, _ = detector.detectMarkers(gray)
+
+
+if ids is not None:
+    aruco.drawDetectedMarkers(image, corners, ids)
+    for i, marker_id in enumerate(ids):
+        print(f"Detected ArUco ID: {marker_id[0]}")
+else:
+    print("No markers detected.")
+
+
+cv2.imshow("Detected ArUco Markers", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
